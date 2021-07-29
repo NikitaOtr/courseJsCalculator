206fe9792dbcc66e14d0d3d37583a3d8dee177b2
NikitaOtr <nikita137174@list.ru>
2021-07-29 15:35:07 +0300

    add var

---
+++ script.js
@@ -0,0 +1,8 @@
+let money;
+const income = 1123;
+const addExpenses = 'Hello';
+let deposit = true;
+let mission = 34.367;
+const period = '12-30';
+alert('Hello world!!!');
+console.log('Hello world!!!');
