fe284922d39a24988c185c35b90031469b820dfc
NikitaOtr <nikita137174@list.ru>
2021-07-30 11:52:35 +0300

    first

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let money;
-const income = 1123;
-const addExpenses = 'Hello';
+let money = 60000;
+const income = 'freelance';
+let addExpenses = 'Taxi, Internet, Communal apartment';
 let deposit = true;
-let mission = 34.367;
-const period = '12-30';
-alert('Hello world!!!');
-console.log('Hello world!!!');
+let mission = 1000000;
+const period = '12';
+
+console.log('money: ', typeof money);
+console.log('income: ', typeof income);
+console.log('deposit: ', typeof deposit);
+
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+
+const budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
+
+
