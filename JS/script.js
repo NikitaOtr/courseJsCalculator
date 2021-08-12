4da3cf3cca407daa431b3e401587ede5b6ce7bdb
NikitaOtr <nikita137174@list.ru>
2021-08-12 12:20:25 +0300

    init

---
+++ JS/script.js
@@ -0,0 +1,35 @@
+const buttonStart = document.getElementById('start');
+// console.log('button: ', buttonStart);
+
+const [plusButton1, plusButton2] = document.getElementsByTagName('button');
+// console.log('plusButton1: ', plusButton1);
+// console.log('plusButton2: ', plusButton2);
+
+const checkBox = document.querySelector('#deposit-check');
+// console.log('checkBox: ', checkBox);
+
+const inputIncome = document.querySelectorAll('.income-items input');
+// console.log('inputIncome: ', inputIncome);
+
+const rightPart1 = document.getElementsByClassName('budget_month-value');
+// console.log('rightPart1: ', rightPart1);
+const rightPart2 = document.getElementsByClassName('budget_day-value');
+// console.log('rightPart2: ', rightPart2);
+const rightPart3 = document.getElementsByClassName('expenses_month-value');
+// console.log('rightPart3: ', rightPart3);
+const rightPart4 = document.getElementsByClassName('additional_expenses-value');
+// console.log('rightPart4: ', rightPart4);
+const rightPart5 = document.getElementsByClassName('income_period-value');
+// console.log('rightPart5: ', rightPart5);
+const rightPart6 = document.getElementsByClassName('target_month-value');
+// console.log('rightPart6: ', rightPart6);
+
+const [otherinput1, otherinput2, otherinput3, otherinput4, otherinput5, otherinput6, otherinput7] = document.querySelectorAll('.result input');
+// console.log(otherinput1, otherinput2, otherinput3, otherinput4, otherinput5, otherinput6, otherinput7);
+
+
+
+
+
+
+
